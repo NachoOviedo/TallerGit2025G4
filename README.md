d2344a402429253ca95058e436fe56e3d0687f48
NachoOviedo <i.oviedo.1@estudiantec.cr>
2025-05-30 10:30:05 -0600

    Update README.md

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # TallerGit2025G4
-Super taller para super poder super git hub
+Super taller para super poder super usar super git super hub
---
+++ arrchivo para subir.py
@@ -0,0 +1,6 @@
+#nombre
+def ingrese_nombre(nombre):
+    if isinstance(nombre,str):
+        return nombre
+    else:
+        return 'error'
