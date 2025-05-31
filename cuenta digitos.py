9dc08d59c50a37ec0e0230248444dc9dff903773
Ignacio <i.oviedo.1@estudiantec.cr>
2025-05-31 08:19:21 -0600

    Reapply "Cuenta digitos"
    This reverts commit fe548d8aee347b426d9601602b8b83df672162bc.

---
+++ cuenta digitos.py
@@ -0,0 +1,12 @@
+def CuentaDigitos(num,dig):
+    if isinstance(num and dig,int) and dig<10 and dig>=0:
+            return CuentaDigitos_Aux(abs(num),dig)
+    else:
+        return "Error este es un numero invalido"
+def CuentaDigitos_Aux(num, dig):
+    if num == 0:
+        return 0
+    elif num%10>dig:
+        return 1+CuentaDigitos_Aux(num//10, dig)    
+    else:
+        return CuentaDigitos_Aux(num//10, dig)
